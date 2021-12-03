65056ab9fec597116a1892a1f5927dbdbc66a551
Jeevitha <jeevithaparandhaman@kanini.com>
2021-12-03 15:33:21 +0530

    Task 2

---
+++ app/src/main/java/com/example/application1/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.application1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import com.google.android.material.textfield.TextInputLayout
+
+class MainActivity : AppCompatActivity() {
+    var clickCount = 0
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val Countview = findViewById<TextView>(R.id.clickCountView)
+        val clickbutton = findViewById<TextView>(R.id.clickButton)
+        val Nametext = findViewById<TextInputLayout>(R.id.Nametext)
+        clickbutton.setOnClickListener {
+
+            val Username = Nametext.editText?.text?.toString()
+            val maskedUserName =
+                if (Username.isNullOrEmpty()) "Somebody"
+                else
+                    Username
+
+            clickCount++
+            Countview.text = "$maskedUserName clicked $clickCount times"
+        }
+    }
+}
